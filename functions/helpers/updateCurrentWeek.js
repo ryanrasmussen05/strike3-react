e6f7151013b741aa6203c255bec0e5d169aa201c
Ryan Rasmussen <ryanrasmussen05@gmail.com>
2019-07-26 15:23:31 -0500

    fix issues with auto updating current week

--- functions/helpers/updateCurrentWeek.js
+++ functions/helpers/updateCurrentWeek.js
@@ -1,25 +1,31 @@
 const getGameData = require('./getGameData').getGameData;
 
+// returns the first week number that has an open pick status
+const getActiveWeek = gameData => {
+  let activeWeek = 1;
+
+  for (let week = 1; week <= 17; week++) {
+    for (const player of gameData.players) {
+      const pick = player.picks[week - 1];
+
+      if (pick.status === 'open') {
+        activeWeek = week;
+        return activeWeek;
+      }
+    }
+  }
+};
+
 // if all results are set for current week, increment the week
 exports.updateCurrentWeek = async(context, database) => {
   const gameData = await getGameData(context, database, true);
 
-  const week = gameData.week;
+  const currentWeek = gameData.week;
+  const desiredCurrentWeek = getActiveWeek(gameData);
 
-  let weekIsComplete = true;
-
-  for (const player of gameData.players) {
-    const pick = player.picks[week - 1];
-
-    if (pick.status === 'open') {
-      weekIsComplete = false;
-      break;
-    }
-  }
-
-  if (weekIsComplete) {
-    await database.ref(`week`).set(week + 1);
-    gameData.week = week + 1;
+  if (desiredCurrentWeek && (currentWeek !== desiredCurrentWeek)) {
+    await database.ref(`week`).set(desiredCurrentWeek);
+    gameData.week = desiredCurrentWeek;
   }
 
   return gameData;
