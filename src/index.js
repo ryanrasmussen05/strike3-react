695da20d9d26cc1acf95aa9e10bc590de06cec8e
Ryan Rasmussen <ryanrasmussen05@gmail.com>
2019-08-12 09:14:05 -0500

    add environment files for deploying to firebase more easily

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import 'firebase/auth';
 import 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJG85EJA1lBmYS31zcOqjSECZ2YSpWHxo',
-  authDomain: 'strike3-31769.firebaseapp.com',
-  databaseURL: 'https://strike3-31769.firebaseio.com',
-  projectId: 'strike3-31769',
-  storageBucket: 'strike3-31769.appspot.com',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
 
 firebase.initializeApp(firebaseConfig);
